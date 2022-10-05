49dcb90b6faced28dc5e0bd856ee291da2e130ad
Hayat Romedan <hayatromedan351@gmail.com>
2022-10-05 14:10:06 -0700

    Create DominoPilling.java

---
+++ DominoPilling.java
@@ -0,0 +1,25 @@
+import java.io.*;
+ 
+class GFG {
+ 
+    static int countWays(int n)
+    {
+        int []A = new int[n+1];
+        int []B = new int[n+1];
+        A[0] = 1; A[1] = 0;
+        B[0] = 0; B[1] = 1;
+        for (int i = 2; i <= n; i++)
+        {
+            A[i] = A[i - 2] + 2 * B[i - 1];
+            B[i] = A[i - 1] + B[i - 2];
+        }
+     
+        return A[n];
+    }
+ 
+    public static void main (String[] args)
+    {
+        int n = 8;
+        System.out.println(countWays(n));
+    }
+}
